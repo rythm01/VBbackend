722b9f7cf7c382e452064c8900bc6f7df2ed4568
Ridham <115993280+rythm01@users.noreply.github.com>
2023-09-09 11:23:12 +0530

    modification

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ process.on("uncaughtException", (err) => {
     process.exit(1);
 })
 app.use(cookieParser());
+app.use(express.static('public'));
 
 const corsOptions = {
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify the allowed HTTP methods
